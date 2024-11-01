24fd805cfdd10d92d82c6fb3748520ee457c1d29
BarkerProoks <jonparkerbrooks@gmail.com>
2024-11-01 16:28:31 -0500

    added local networking to simplify for now

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
-#include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "cyw43_config.h"
 
 #include <stdio.h>
 
@@ -10,13 +11,12 @@
 
 
 int main() {
-    // networking 
+    // below: networking (AP version, the robot can host its own network)
+    // still under construction
+#ifdef AP_MODE
     ip4_addr_t gateway, netmask; // we are the gateway
     struct dhcp_server dhcpd; // allows the pi to assign IP addresses
     struct dns_server dnsd; // allows host discovery
-
-    // meatspace
-    struct sixaxis sensor; // the only sensor we have, 6axis gyro + accelerometer
 
     cyw43_arch_init(); // enable networking kernel
     stdio_init_all();  // allow STDOUT over USB
@@ -28,6 +28,24 @@ int main() {
     IP4_ADDR(&gateway, 192, 168, 0, 1); // _gateway on the first address
 
     dhcp_server_init(&dhcpd, gateway);
+#else
+    // just connect to local network for now
+    if (cyw43_arch_init()) {
+        printf("no networking :(\n");
+        return 1;
+    }
+
+    cyw43_arch_enable_sta_mode();
+    
+    if (cyw43_arch_wifi_connect_timeout_ms("ssid", "password", CYW43_AUTH_WPA3_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect\n");
+    } else {
+        printf("connected!\n");
+    }
+#endif
+
+    // meatspace
+    struct sixaxis sensor; // the only sensor we have, 6axis gyro + accelerometer
 
     // initialize peripherials
     sixaxis_init();
