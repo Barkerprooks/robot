2caa5823c7d8831e161eb8830f957b45095d9945
BarkerProoks <jonparkerbrooks@gmail.com>
2024-11-01 07:35:34 -0500

    write request bytes are not required in the `i2c_write_blocking` method

--- src/sixaxis.c
+++ src/sixaxis.c
@@ -5,8 +5,8 @@
 
 #include <stdint.h>
 
-#define SPI_SDA_PIN 4 // a.k.a the TX pin
-#define SPI_SCK_PIN 5 // a.k.a the clock pin
+#define I2C_SDA_PIN 4
+#define I2C_SCK_PIN 5
 
 #define MPU6050_DEVICE_ID 0x68
 #define MPU6050_ACCEL_REG 0x3b
@@ -22,10 +22,10 @@ void init_sixaxis() {
     i2c_init(i2c0, 400000); // 400 KHz
 
     // set up the TX pin and clock pin to pull up by default
-    gpio_set_function(SPI_SDA_PIN, GPIO_FUNC_I2C);   
-    gpio_set_function(SPI_SCK_PIN, GPIO_FUNC_I2C); 
-    gpio_pull_up(SPI_SDA_PIN);
-    gpio_pull_up(SPI_SCK_PIN);
+    gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);   
+    gpio_set_function(I2C_SCK_PIN, GPIO_FUNC_I2C); 
+    gpio_pull_up(I2C_SDA_PIN);
+    gpio_pull_up(I2C_SCK_PIN);
 }
 
 bool read_sixaxis(struct sixaxis *data) {
@@ -50,7 +50,7 @@ bool read_sixaxis(struct sixaxis *data) {
 }
 
 void reset_sixaxis() {
-    // 0x6b = reset, 0x80 = write request
-    uint8_t reset_bytes[2] = { 0x6b, 0x80 };
+    // 0x6b = reset, 0x00 = write request
+    uint8_t reset_bytes[2] = { 0x6b, 0x00 };
     i2c_write_blocking(i2c0, MPU6050_DEVICE_ID, reset_bytes, 2, false);
 }
