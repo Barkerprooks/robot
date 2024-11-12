e7fdb1fe02b2f87b864d0d6d4531da0bf75cb4c5
BarkerProoks <jonparkerbrooks@gmail.com>
2024-11-11 18:53:35 -0600

    progress

--- src/main.c
+++ src/main.c
@@ -25,12 +25,12 @@ int main() {
 
     //network_init(&robot, WIFI_SSID, WIFI_PASSWORD, 5);
     //service_init(&robot, &server, 42069);
-    sixaxis_init(&robot, &sensor, GYRO_FREQ_250, ACCEL_FREQ_16);
+    sixaxis_init(&robot, &sensor, GYRO_FREQ_250, ACCEL_FREQ_2);
 
     // no offset is good enough for now
-    //sixaxis_set_offset(ACCEL_Y_OFFSET, 0);
-    //sixaxis_set_offset(ACCEL_Z_OFFSET, 0);
-    //sixaxis_set_offset(GYRO_X_OFFSET, 0);
+    sixaxis_set_offset(ACCEL_Y_OFFSET, 1593);
+    sixaxis_set_offset(ACCEL_Z_OFFSET, 1000);
+    sixaxis_set_offset(GYRO_X_OFFSET, 58);
     
     while (running(robot)) {
         start = (double) time_us_64() / 1000.0;
@@ -41,11 +41,19 @@ int main() {
         delta = (stop - start) * 0.001;
 #if DEBUG
         printf("network: %s\n", robot.network == NO_ERROR ? "connected" : "disconnected");
-        printf("sixaxis: %s\n", robot.sixaxis == NO_ERROR ? "enabled" : "disabled");
+        printf("sixaxis: %s\n\n", robot.sixaxis == NO_ERROR ? "enabled" : "disabled");
+
+        printf(" gyro x: %f\n", (double) sensor.gyro.x * sensor.gyro.resolution);
+        printf("accel y: %f\n", (double) sensor.accel.y * sensor.accel.resolution);
+        printf("accel z: %f\n", (double) sensor.accel.z * sensor.accel.resolution);
+        printf("angle: %f\n\n", sensor.angle);
+
         printf("speed: %d\n", robot.speed);
-        printf("angle: %f\n", sensor.angle);
+        printf("delta: %f\n", delta);
+
         sleep_ms(DEBUG_PRINT_DELAY); // delay for the clear screen
         printf("\033[2J\033[H"); // clear screen before writing, we can monitor the values easier that way
 #endif
     }
+
 }
